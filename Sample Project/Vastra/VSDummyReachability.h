a0cd39b83562175a256f87869f1ce2e9f3e028a2
Paolo Tagliani <paolo@mobilejazz.com>
2016-12-22 17:05:11 +0100

    Added VSReachebilityProtocol to decouple reachability from Vastra

---
+++ Sample Project/Vastra/VSDummyReachability.h
@@ -0,0 +1,15 @@
+//
+//  VSDummyReachability.h
+//  Vastra
+//
+//  Created by Paolo Tagliani on 22/12/16.
+//  Copyright © 2016 Mobile Jazz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "VSReachabilityProtocol.h"
+
+@interface VSDummyReachability : NSObject <VSReachabilityProtocol>
+
+@end
---
+++ Sample Project/Vastra/VSDummyReachability.m
@@ -0,0 +1,18 @@
+//
+//  VSDummyReachability.m
+//  Vastra
+//
+//  Created by Paolo Tagliani on 22/12/16.
+//  Copyright © 2016 Mobile Jazz. All rights reserved.
+//
+
+#import "VSDummyReachability.h"
+
+@implementation VSDummyReachability
+
+- (BOOL)isReachable
+{
+    return YES;
+}
+
+@end
